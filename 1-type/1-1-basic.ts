dfa3c74f4ca893c1aa9580473934ade48ea96217
mason park <joung4342@gmail.com>
2022-07-27 20:35:14 +0900

    기본타입정리2

--- 1-type/1-1-basic.ts
+++ 1-type/1-1-basic.ts
@@ -36,4 +36,43 @@
     // null : 값이 비어있음
     let person : null; //null 밖에 할당할 수 없기때문에 이런식으로 사용하지 않는다.
     let person2: string | null;
+
+    // unknown 어떤 종류의 데이터 타입이 올지 모른다.
+    // 되도록이면 사용하지 말자!
+    // 존재 이유 : 타입 스크립트에 자바스크립트 라이브러리를 사용할때  어떤 타입인지 잘 모를때 사용한다.
+    // 하지만 이때도 되도록 타입을 정해 주는 것이 좋다.
+    let notSure:unknown = 0;
+    notSure = 'not sure';
+    notSure = true;
+
+    // any : 어떤 타입이든 전부 담을 수 있다!
+    // 되도록이면 사용하지 말자!
+    let anything: any = 0;
+    anything = 'anything';
+
+    // void return 값이 없다.
+    function print():void {
+        console.log('hello');
+        return;
+    }
+
+    //  never : return을 절대 하지 않겠다!
+    function throwError(message:string):never {
+        // message -> server (log) 서버에 전달해서 로그를 남긴다.
+        throw Error(message);
+    /*    while (true) {
+
+        }*/
+    }
+
+    // object : 원시 타입을 제외한 모든 타입을 담을 수 있다.
+    // 이런식으로 포괄적인 타입은 사용하지 않는 것이 좋다.
+    let obj : object;
+
+    function acceptSomeObject(obj: object) {
+
+    }
+
+    acceptSomeObject({name: 'mason'});
+    acceptSomeObject({animal: 'dog'});
 }
