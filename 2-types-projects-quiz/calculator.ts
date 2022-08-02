05d0758a922d1c7ab19ab74aea43603f101f7109
mason park <joung4342@gmail.com>
2022-08-02 20:04:29 +0900

    my calculator

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,6 +1,23 @@
 /**
  * Let's make a calculator 🧮
  */
+type Operator = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+
+function calculate(opperator:Operator,number1:number,number2:number): number {
+    switch (opperator) {
+        case 'add':
+            return number1 + number2;
+        case 'substract':
+            return number1 - number2;
+        case 'multiply' :
+            return number1 * number2;
+        case 'divide' :
+            return number1 / number2;
+        case 'remainder':
+            return number1 % number2;
+    }
+}
+
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
