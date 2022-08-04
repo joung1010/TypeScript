59c7d37b50ec3de3489da9e9feac5cbe06a8234e
mason park <joung4342@gmail.com>
2022-08-04 20:35:31 +0900

    getter setter

--- 3-oop/3-3-encapsulation.ts
+++ 3-oop/3-3-encapsulation.ts
@@ -11,7 +11,7 @@
        private static BEANS_GRAMM_PER_SHOT :number  = 7; // class level
         private _coffeeBeansGramm: number;   // instance (object) level
 
-       private constructor(coffeeBeansGramm) {
+       private constructor(coffeeBeansGramm:number) {
             this._coffeeBeansGramm=  coffeeBeansGramm;
         }
 
@@ -41,4 +41,30 @@
     const maker =  CoffeeMaker.makeMachine(32);
     maker.fillCoffeeBenas(32);
 
+    class User {
+        get fullName():string {
+            return `${this.fisrtName} ${this.lastName}`;
+        }
+        private interAge  = 4;
+
+        get age() : number {
+            return this.interAge;
+        }
+
+        set age(age:number) {
+            if (age < 0) {
+                throw  new Error('vale for age should be greater than 0');
+            }
+            this.interAge = age;
+        }
+
+        // 생성자 함수의 파라미터에 접근제어를 설정하면 별도로 설정할 필요가 없다.
+        constructor(private fisrtName: string,private lastName: string) {
+        }
+    }
+
+    const user = new User('steve', 'jobs');
+    user.age = 4;
+    console.log(user.fullName);
+    console.log(user.age)
 }
