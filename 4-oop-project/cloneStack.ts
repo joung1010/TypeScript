a7af4c25365e963091cb770ff50e6a3c1b7be9d6
mason park <joung4342@gmail.com>
2022-08-10 13:12:23 +0900

    clone stack

---
+++ 4-oop-project/cloneStack.ts
@@ -0,0 +1,52 @@
+{
+    interface Stack {
+        readonly size: number;
+        push(value: string):void;
+        pop(): string;
+    }
+
+    type StackNode = {
+        readonly value:string;
+        readonly next?: StackNode ; // StackNode | undefined
+    }
+
+    class StackImpl implements Stack {
+        private _size:number = 0;
+        private head?: StackNode;
+
+        constructor(private capacity:number) {
+        }
+
+        get size() {
+            return this._size;
+        }
+
+        push(value: string) {
+            if (this._size === this.capacity) {
+                throw new Error('Stack is full');
+            }
+            this._size++;
+            const node: StackNode = {value, next: this.head};
+            this.head = node;
+        }
+        pop(): string { // null == undefined
+            if (this.head == null) {
+                throw new Error('Stack is Empty!');
+            }
+            const node  = this.head;
+            this.head = node.next;
+            this._size--;
+            return node.value;
+        }
+
+    }
+
+    const stack = new StackImpl(5);
+    stack.push('Bob1');
+    stack.push('Bob2');
+    stack.push('Bob3');
+    while (stack.size !== 0) {
+        console.log(stack.pop());
+    }
+
+}
