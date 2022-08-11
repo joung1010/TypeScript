59a2e744d87e0dd9fab0db48ebef723a2c6434b1
mason park <joung4342@gmail.com>
2022-08-11 19:23:15 +0900

    Read Array Interface API

---
+++ 7-API/7-1-api.ts
@@ -0,0 +1,42 @@
+// https://github.com/microsoft/TypeScript/blob/master/lib/lib.es5.d.ts
+Array;
+[1.2].map
+
+type Student = {
+    passed: boolean;
+}
+
+const students:Student[] = [
+    {passed:true},
+    {passed:true},
+    {passed:true},
+    // {passed:false},
+];
+
+const result = students.every(student => student.passed);
+console.log(result);
+
+// every<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): this is S[];
+
+
+class Animal {}
+
+class Cat extends Animal {
+    isCat: boolean = true;
+}
+
+class Dog extends Animal {
+    isDog:boolean = false;
+}
+
+const animals : Animal[] = [
+    new Cat(),
+    new Cat(),
+    // new Dog(),
+]
+
+function isCat(animal: Animal): animal is Cat {
+    return (animal as Cat).isCat !== undefined;
+}
+
+console.log(animals.every<Cat>(isCat));
