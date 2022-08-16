a150dd46db772d34b82bb879bac6aa5edaf67db9
mason park <joung4342@gmail.com>
2022-08-16 20:25:41 +0900

    Type 과 Interface 의 차이(개념 측면)

--- 9-advanced/9-1-interface.ts
+++ 9-advanced/9-1-interface.ts
@@ -63,4 +63,23 @@
     type Name = Person['name']; // Name 타입은 Person 안에 있는 name property의 타입을 사용한다.(string)
     type NumberType = number;
     type Direction = 'left' | 'right'; // union type
+
+    // 4. 개념적인 측면에서의 차이점
+    // interface 의 정의 :  어떤 것에 대한 규격사항, object 와 object의 의사소옽에서 정해진 interface 규격에 따라서 의사소통한다.
+    // 따라서 어떤 특정한 규격을 정하고 그 규격을 통해서 구현된다면 (ex) 커피머신 인터페이스, 라떼 머신, 비싼 커피머신등...)
+    // 인터페이스를 사용하는 것이 더 정확하다.
+
+    //Types : 어떠한 데이터를 담을때 그 데이터의 모습을 정의하는 것이다.
+    /*
+    *   type PositionType = {
+        x: number;
+        y: number;
+    }
+    *   const pos: PositionType = {
+        x: 1,
+        y: 1,
+    }
+    * */
+    // 만약 위의 예제를 interface로 정의해서 구현한다면 보는사람 입장에서 Postion이라는 class가 존재하나?? 라고 생각할 수 있다.
+    // 따라서 어떠한 것을 구현하는게 목적이아니라 데이터를 담는 것을 목적으로 한다면 Type을 사용하는 것이 정확하다.
 }
